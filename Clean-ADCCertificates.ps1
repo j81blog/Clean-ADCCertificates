46bcef091abb72cceb4c9eb3467f62773ae9ed51
John B <john@j81.nl>
2020-11-05 22:24:04 +0100

    Update Clean-ADCCertificates.ps1

--- Clean-ADCCertificates.ps1
+++ Clean-ADCCertificates.ps1
@@ -3,7 +3,8 @@
 $text = @"
 
 #Specify the parameters
-$Params = @{
+
+`$Params = @{
     ManagementURL = "https://citrixadc.domain.local"
     Credential = Get-Credential -UserName "nsroot" -Message "Citrix ADC account"
     Backup = $true
